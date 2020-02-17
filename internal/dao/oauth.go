e648d17dfe8f7c4a1f1bda2c1e1930f0cdf5426e
王刚 <withwanggang@gmail.com>
2020-02-17 17:17:49 +0800

    fix

--- internal/dao/oauth.go
+++ internal/dao/oauth.go
@@ -13,10 +13,9 @@ var Oauth = new(oauth)
 func (*oauth) GetUID(openID string) (uid string, err error) {
 	var appID string
 	if config.Server.Env == "test" {
-		// appID = "04565e551f7ff066"
-		appID = "zd7b3n7nazce89bf"
+		appID = "04565e551f7ff066"
 	} else {
-		appID = "zd7b3n7nazce89bf"
+		appID = ""
 	}
 
 	row := db.MysqlCli.QueryRow("select uid from oauth where open_id = ? and app_id = ?",

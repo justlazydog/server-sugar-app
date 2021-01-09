d58e25ad45aa5545d6d60b6aa73a9fce0517302c
王刚 <withwanggang@gmail.com>
2021-01-09 11:08:14 +0800

    fix RewardDetail.Get

--- internal/dao/sugar.go
+++ internal/dao/sugar.go
@@ -143,11 +143,11 @@ func (*rewardDetail) Get(userID string) (res model.RewardDetail, err error) {
 	str := t.Format("2006-01-02") + " 00:00:00"
 	t, _ = time.ParseInLocation("2006-01-02 15:04:05", str, loc)
 
-	sqlStr := "select create_time,today_bal,growth_rate,balance_hash_rate,invite_hash_rate from reward_detail where " +
+	sqlStr := "select create_time,today_bal,destroy_hash_rate,growth_rate,balance_hash_rate,invite_hash_rate from reward_detail where " +
 		"user_id = ? and create_time >= ? order by id desc limit 1"
 	row := db.MysqlCli.QueryRow(sqlStr, userID, t.In(time.Local))
 	var createTime string
-	err = row.Scan(&createTime, &res.TodayBal, &res.GrowthRate, &res.BalanceHashRate, &res.InviteHashRate)
+	err = row.Scan(&createTime, &res.TodayBal, &res.DestroyHashRate, &res.GrowthRate, &res.BalanceHashRate, &res.InviteHashRate)
 	if err == sql.ErrNoRows {
 		res.CreateTime = time.Now()
 		return res, nil
